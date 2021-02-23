0efde27935708c835761b2ab4f51bb3c53b1573f
rvin1 <78807759+rvin1@users.noreply.github.com>
2021-02-23 13:19:19 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft {
    file { '/opt/minecraft':
      ensure => directory,
    }
-   file { '/opt/minecraft/minecraft_server.job':
+   file { '/opt/minecraft/minecraft_server.jar':
      ensure => file,
      source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
    }
